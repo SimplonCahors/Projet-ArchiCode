923218cb1afb5c3e34c1e59bd46cd3215ff4befd
nico <nicocofer@hotmail.com>
2018-01-31 15:38:53 +0100

    reparation du gulpfile pour watch les components et global

--- web/themes/custom/cplus2b/gulpfile.js
+++ web/themes/custom/cplus2b/gulpfile.js
@@ -10,6 +10,8 @@ var minify = require('gulp-minify');
 
 gulp.task('default', ['compress','sass'],function() {
 	gulp.watch('assets/scss/*.scss', ['sass']);
+    gulp.watch('assets/scss/components/*.scss', ['sass']);
+    gulp.watch('assets/scss/global/*.scss', ['sass']);
 	gulp.watch('assets/js/*.js', ['compress']);
 
 });
