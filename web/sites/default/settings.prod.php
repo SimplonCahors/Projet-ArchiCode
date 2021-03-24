e668a13153bac4afb20a73de1ded7ce42fecb570
Raphael Apard <raphael.apard@acolad.fr>
2021-03-24 12:24:05 +0100

    Fix mailer

--- web/sites/default/settings.prod.php
+++ web/sites/default/settings.prod.php
@@ -14,6 +14,17 @@ $config['system.performance']['js']['preprocess'] = TRUE;
 $config['system.performance']['cache']['page']['max_age'] = 86400;
 
 /**
+ * Mail
+ */
+$config['swiftmailer.transport']['transport'] = 'smtp';
+$config['swiftmailer.transport']['smtp_host'] = 'ssl0.ovh.net';
+$config['swiftmailer.transport']['smtp_port'] = '587';
+$config['swiftmailer.transport']['smtp_encryption'] = 'tls';
+$config['swiftmailer.transport']['smtp_credential_provider'] = 'swiftmailer';
+$config['swiftmailer.transport']['smtp_credentials']['swiftmailer']['username'] = 'no-reply@docteur-conso.fr';
+$config['swiftmailer.transport']['smtp_credentials']['swiftmailer']['password'] = "!.XMvT*3s%@'";
+
+/**
  * Disable automated cron
  */
 $config['automated_cron.settings']['interval'] = 0;
